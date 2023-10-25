a0e0000d0daee9065bb74d8baab1ab33bffee0b5
André Codo <67808063+AcCodo@users.noreply.github.com>
2023-10-24 22:14:24 -0300

    Removed unnecessary Expanded

--- lib/MTGCardWidget.dart
+++ lib/MTGCardWidget.dart
@@ -8,35 +8,33 @@ class MTGCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: 500,
-              child: Image.network(
-                card!.imageUris!.large!,
-                fit: BoxFit.scaleDown,
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 500,
+            child: Image.network(
+              card!.imageUris!.large!,
+              fit: BoxFit.scaleDown,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                    child: Text(
-                  card!.name!,
-                  style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Text(card!.colorIdentity!.join())],
-                )
-              ],
-            )
-          ],
-        ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                  child: Text(
+                card!.name!,
+                style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text(card!.colorIdentity!.join())],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
